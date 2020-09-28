3d45f4a0ff5983d27f15ef29cc24217c04f7f8b5
LindaSt <linda.studer@unifr.ch>
2020-09-28 17:55:08 +0200

    Tested for one file and ResNet18

--- data_loader.py
+++ data_loader.py
@@ -14,10 +14,11 @@ import torch
 from torchvision.datasets.folder import pil_loader
 from torchvision import transforms
 
+
 class ImageDataset(Dataset):
     """Face Landmarks dataset."""
 
-    def __init__(self, root_dir, img_type='png', file_identifier=''):
+    def __init__(self, root_dir, expected_input_size, img_type='png', file_identifier=''):
         """
         Args:
             root_dir (string): Directory with all the images.
@@ -25,30 +26,27 @@ class ImageDataset(Dataset):
                 on a sample.
         """
         self.root_dir = root_dir
-
+        self.expected_input_size = expected_input_size
         self.img_type = img_type
         self.file_identifier = file_identifier
 
     @property
     def file_list(self):
-        return [os.path.basename(f) for f in glob.glob(os.path.join(self.root_dir, f'*{self.file_identifier}*.{self.img_type}'))]
+        return [os.path.basename(f) for f in
+                glob.glob(os.path.join(self.root_dir, f'*{self.file_identifier}*.{self.img_type}'))]
 
     def __len__(self):
         return len(self.file_list)
 
     def __getitem__(self, idx):
         img_name = self.file_list[idx]
-        image = np.asarray(pil_loader(os.path.join(self.root_dir, img_name)))
-        # # swap color axis because
-        # # numpy image: H x W x C
-        # # torch image: C X H X W
-        # image = np.transpose(image, (2, 0, 1))
-        # TODO: add resize transforamtion to expected input size of network (see how it's done in Gale)
-        transformations = transforms.Compose([transforms.ToTensor(),
-                                              transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        image = pil_loader(os.path.join(self.root_dir, img_name))
+        # resize images to expected input size (depends on model) and normalize according to ImageNet normalization
+        pre_process = transforms.Compose([transforms.Resize(self.expected_input_size),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
         # make a tensor out of the image and normalize according to ImageNet dataset
-        image = transformations(image)
+        image = pre_process(image)
 
         return image, img_name
-
--- feature_extraction.py
+++ feature_extraction.py
@@ -2,9 +2,11 @@ import fire
 import os
 import pandas as pd
 import numpy as np
+import re
+import encoder_models.ResNet as resnet
+import encoder_models.VGG as vgg
 
 import torch.nn as nn
-from torchvision import models
 import torch
 from torch.utils.data import DataLoader
 
@@ -16,76 +18,69 @@ class FeatureExtractor:
 
 
     """
-    def __init__(self, imgs_path: str, model_name: str, file_identifier: str = '', img_type: str = 'png', batch_size: int = 32):
+
+    def __init__(self, imgs_path: str, model_name: str, img_type: str = 'png',
+                 batch_size: int = 32):
         self.imgs_path = imgs_path
         self.model_name = model_name
         self.img_type = img_type
 
         self.batch_size = batch_size
-        self.file_identifier = file_identifier
 
     @property
     def model(self):
-        # TODO: change and do like in Gale (copy models from Gale)
-        model_fct = eval(f'models.{self.model_name}')
+        # classifier is removed in the forward pass (see file in encoder_models)
+        model_category = re.match('\D*', self.model_name).group(0)
+        model_fct = eval(f'{model_category}.{self.model_name}')
         model = model_fct(pretrained=True)
-
-        # remove last fully-connected layer
-        new_classifier = nn.Sequential(*list(model.children())[:-1])
-        model.classifier = new_classifier
 
         return model
 
     @property
     def data(self):
-        return ImageDataset(self.imgs_path, self.img_type, self.file_identifier)
+        return ImageDataset(root_dir=self.imgs_path, expected_input_size=self.model.expected_input_size, img_type=self.img_type)
 
     def get_features(self):
         dataloader = DataLoader(self.data, batch_size=self.batch_size, num_workers=0)
-        features = np.array([])
-        img_names = np.array([])
+        features = {}
         for i_batch, (img_batch, filename_batch) in enumerate(dataloader):
             self.model.eval()
             # Forward Pass
             with torch.no_grad():
                 output_batch = self.model(img_batch)
                 # save the output features
-                img_names = np.append(img_names, list(filename_batch))
-                features = np.append(features, np.array(output_batch))
+                assert len(output_batch) == len(filename_batch)
+                for fn, output in zip(list(filename_batch), output_batch):
+                    features[fn] = np.array(output)
 
-        return features, img_names
+        return features
 
-def extract_features(imgs_path: str, output_path: str, model: str, file_identifiers: list = [],
-                     multifolder: bool = False, img_type: str = 'png'):
+
+def extract_features(imgs_path: str, output_path: str, model: str, multifolder: bool = False, img_type: str = 'png'):
     """
 
-    :param file_identifier:
     :param imgs_path:
     :param output_path:
     :param model:
     :param multifolder:
+    :param img_type:
     """
-
     if not multifolder:
         subfolders = [imgs_path]
     else:
-        subfolders = [os.path.join(imgs_path, o) for o in os.listdir(imgs_path) if os.path.isdir(os.path.join(imgs_path, o))]
+        subfolders = [os.path.join(imgs_path, o) for o in os.listdir(imgs_path) if
+                      os.path.isdir(os.path.join(imgs_path, o))]
 
-    feature_table = np.array([])
-
+    features = {}
     for folder in subfolders:
-        features = np.array([])
-        for file_identifier in file_identifiers:
-            feature_extractor = FeatureExtractor(imgs_path=folder, model_name=model, img_type=img_type, file_identifier=file_identifier)
-            f = feature_extractor.get_features()
-
-            features = np.append(features, f)
+        feature_extractor = FeatureExtractor(imgs_path=folder, model_name=model, img_type=img_type)
+        feature_dict = feature_extractor.get_features()
+        features = {**features, **feature_dict}
 
     # create the data frame and save it
-    df = pd.DataFrame(feature_table)
-    df.to_csv(os.path.join(output_path, f'{model}-patch-features'.png))
+    df = pd.DataFrame.from_dict(features, orient='index')
+    df.to_csv(os.path.join(output_path, f'{model}-patch-features.csv'))
 
 
 if __name__ == '__main__':
     fire.Fire(extract_features)
-
